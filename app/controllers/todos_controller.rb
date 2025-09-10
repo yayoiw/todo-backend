450a0072b01cc2610d0893d414e4a24eb2803c3a
yayoiwatabe <lemo.pep@gmail.com>
2025-09-10 13:14:06 +0900

    add: create action

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -3,4 +3,18 @@ class TodosController < ApplicationController
     todos = Todo.all
     render json: todos
   end
+
+  def create
+    todo = Todo.new(todo_params)
+    if todo.save
+      render json: todo, status: :created
+    else
+      render json: todo.errors, status: :unprocessable_entity
+    end
+  end
+  private
+
+    def todo_params
+      params.require(:todo).permit(:title, :completed)
+    end
 end
